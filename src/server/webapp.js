0d887364e79bc4cd9679f17a13f272ef465b6e49
Liang Liang <lilysway@hotmail.com>
2020-07-24 19:42:57 -0400

    student form and instructor form created

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -1,5 +1,25 @@
-const doGet = () => {
-  const title = 'Google Apps Script';
+const Route = {};
+Route.path = (route, callback) => {
+  Route[route] = callback;
+};
+
+function loadStudent() {
+  return HtmlService.createHtmlOutputFromFile('student.html');
+}
+
+function loadInstructor() {
+  return HtmlService.createHtmlOutputFromFile('instructor.html');
+}
+
+const doGet = (e) => {
+  Route.path('student', loadStudent);
+  Route.path('instructor', loadInstructor);
+
+  if (Route[e.parameters.v]) {
+    return Route[e.parameters.v]();
+  }
+
+  const title = 'Workshop Feedback Form';
   const fileName = 'index.html';
   return HtmlService.createHtmlOutputFromFile(fileName)
     .setTitle(title)
